074fda0883a5218ac777f1121734c30b9b200daa
stefany281 <quadros.oliveira.stefany@escola.pr.gov.br>
2026-04-27 09:29:11 -0300

    cc

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
 
 for (let i = 0; i < botoes.length; i++) {
-  console.log(i);
+  botoes[i].onclick = function () {
+  botoes[i].classList.add("ativo");
+};
 }
